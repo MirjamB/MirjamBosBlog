6ad1e49a9a59c2b7469801bbe7c10962cc0e08b7
Mirjam <mirjambos412@gmail.com>
2020-08-18 12:28:29 +0200

    Basic Vue

---
+++ my-blog/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <header> 
+      <h1> Mirjam Bos </h1>
+    </header>
+
+    <!-- Hier komen de blogposts --> 
+    <div>
+      <blog-posts></blog-posts>
+    </div>
+    <footer> 
+      <p> Dit is tekst voor de footer </p>
+    </footer> 
+
+  </div>
+</template>
+
+<script>
+import BlogPost from './components/BlogPost.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'blog-posts': BlogPost,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-blog/src/components/BlogPost.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="blog-posts">
+        <li v-for="post in posts" v-bind:key="post">
+            <h1> {{post.title}} </h1>
+            <p> {{post.text}} </p>
+            <p> {{post.date}} </p>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            posts: [{
+                title: 'Mijn titel',
+                text: 'dit is de tekst van mijn hbagghelkgjlkev',
+                date: '18 aug 2020',
+                },
+            {   title: 'Blog post 2',
+                text: 'Hiet staat de tekst van de tweede blogpost',
+                date: '1 juli 2019',
+                }]
+    
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
